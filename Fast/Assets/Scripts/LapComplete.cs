0da7e7ff6bce59a5313a4a291c8df4ea01e3fc5d
wiktorderen <wiktor1474@wp.pl>
2021-01-04 11:24:48 +0100

    new features
    corrected sun, timer does not stops

---
+++ Fast/Assets/Scripts/LapComplete.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapComplete : MonoBehaviour
+{
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+
+    public GameObject MinuteDisplay;
+    public GameObject SecondDisplay;
+    public GameObject MiliDisplay;
+
+    public GameObject LapTimeBox;
+
+    void OnTiggerEnter()
+    {
+        if(LapTime.SecondCount <= 9)
+        {
+            SecondDisplay.GetComponent<Text>().text = "0" + LapTime.SecondCount + ",";
+        }
+        else
+        {
+            SecondDisplay.GetComponent<Text>().text = "" + LapTime.SecondCount + ",";
+        }
+
+
+        if (LapTime.MinuteCount <= 9)
+        {
+            MinuteDisplay.GetComponent<Text>().text = "0" + LapTime.MinuteCount + ",";
+        }
+        else
+        {
+            MinuteDisplay.GetComponent<Text>().text = "" + LapTime.MinuteCount + ",";
+        }
+
+        MiliDisplay.GetComponent<Text>().text = "" + LapTime.MiliSecCount;
+
+        LapTime.MinuteCount = 0;
+        LapTime.SecondCount = 0;
+        LapTime.MiliSecCount = 0;
+
+        HalfLapTrig.SetActive(true);
+        LapCompleteTrig.SetActive(false);
+    }
+}
---
+++ Fast/Assets/Scripts/LapTime.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTime : MonoBehaviour
+{
+    public static int MinuteCount;
+    public static int SecondCount;
+    public static float MiliSecCount;
+    public static string MiliDisplay;
+
+    public GameObject MinuteBox;
+    public GameObject SecondBox;
+    public GameObject MiliBox;
+
+    void Update()
+    {
+        MiliSecCount += Time.deltaTime * 10;
+        MiliDisplay = MiliSecCount.ToString("F0");
+        MiliBox.GetComponent<Text>().text = "" + MiliDisplay;
+
+        if(MiliSecCount >= 9)
+        {
+            MiliSecCount = 0;
+            SecondCount += 1;
+        }
+
+
+        if(SecondCount <=9)
+        {
+            SecondBox.GetComponent<Text>().text = "0" + SecondCount + ",";
+        }
+        else
+        {
+            SecondBox.GetComponent<Text>().text = "" + SecondCount + ",";
+        }
+
+
+        if(SecondCount >= 60)
+        {
+            SecondCount = 0;
+            MinuteCount += 1;
+        }
+
+
+        if(MinuteCount <= 9)
+        {
+            MinuteBox.GetComponent<Text>().text = "0" + MinuteCount + ":";
+        }
+        else
+        {
+            MinuteBox.GetComponent<Text>().text = "" + MinuteCount + ":";
+        }
+    }
+}
