034b6458fec87d2fb97ab2295809a4a7b589a495
wiktorderen <wiktor1474@wp.pl>
2021-01-15 18:18:00 +0100

    positions
    player is able to see his position

---
+++ Fast/Assets/Scripts/PositionDown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionDown : MonoBehaviour
+{
+    public GameObject positionDisplay;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "CarPosition")
+        {
+            positionDisplay.GetComponent<Text>().text = "2nd place";
+        }
+    }
+}
---
+++ Fast/Assets/Scripts/PositionUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionUp : MonoBehaviour
+{
+    public GameObject positionDisplay;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "CarPosition")
+        {
+            positionDisplay.GetComponent<Text>().text = "1st place";
+        }
+    }
+}
